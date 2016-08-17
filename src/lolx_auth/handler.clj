7e764b72726b945bdbf64524fbcbe17a4dff2bcf
Slawomir Trzaszczka <slawomir.trzaszczka@allegrogroup.com>
2016-08-17 10:55:12 +0200

    rsa files in resource folder

--- src/lolx_auth/handler.clj
+++ src/lolx_auth/handler.clj
@@ -1,10 +1,12 @@
 (ns lolx-auth.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [compojure.handler :refer [site]]
             [lolx-auth.register :refer [register]]
             [lolx-auth.authentication :refer [auth]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]))
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+            [ring.adapter.jetty :as jetty]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
@@ -17,3 +19,8 @@
       (wrap-json-response)
       (wrap-json-body {:keywords? true :bigdecimals? true})
       (wrap-defaults (assoc-in site-defaults [:security] {:anti-forgery false}))))
+
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
